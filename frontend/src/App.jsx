b5a25addfe313f719c4b209694d87383f98e2c5b
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-21 23:57:48 -0400

    fix typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 export default function App() {
-  const BASE_URL = {(process.env.NODE_ENV == 'production') ? 'https://mern-template-restaurant.onrender.com' : 'http://localhost:5174'}
+  const BASE_URL = (process.env.NODE_ENV == 'production') ? 'https://mern-template-restaurant.onrender.com' : 'http://localhost:5174'
   const [dinnerItems, setDinnerItems] = useState([])
   const getDinnerItems = ()=>{
     fetch(`${BASE_URL}/api/dinner`)
